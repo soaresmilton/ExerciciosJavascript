1a889186792063e1a82d887d192bab1b894639fb
soaresmilton <eng.milton.soares@gmail.com>
2020-11-05 11:48:52 -0300

    Exercicio 02

---
+++ Ex02/script.js
@@ -0,0 +1,10 @@
+function calcular() {
+  var r = document.getElementById('txtr')
+  var res = document.getElementById('res')
+  var pi = 3.14159
+
+  var raio = Number(r.value)
+  var area = pi * (raio ** 2)
+  
+  res.innerHTML = `A Área do círculo de raio ${raio} cm é igual: <br> A = ${area} cm³.`
+}
